d5d83916d5b1790e65ae6cd94e2481489bb757a7
zzacnguyen <zzacnguyen@gmail.com>
2018-05-10 22:57:54 +0700

    Cập nhật code cho thông báo

--- app/src/main/java/com/example/zzacn/vnt_mobile/View/Notification/NotificationFragment.java
+++ app/src/main/java/com/example/zzacn/vnt_mobile/View/Notification/NotificationFragment.java
@@ -1,23 +1,108 @@
 package com.example.zzacn.vnt_mobile.View.Notification;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.zzacn.vnt_mobile.Adapter.EventAdapter;
+import com.example.zzacn.vnt_mobile.Adapter.HttpRequestAdapter;
+import com.example.zzacn.vnt_mobile.Config;
+import com.example.zzacn.vnt_mobile.Helper.JsonHelper;
+import com.example.zzacn.vnt_mobile.Interface.OnLoadMoreListener;
+import com.example.zzacn.vnt_mobile.Model.ModelEvent;
+import com.example.zzacn.vnt_mobile.Model.Object.Event;
 import com.example.zzacn.vnt_mobile.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 
 public class NotificationFragment extends Fragment {
+
+    ArrayList<String> finalArr = new ArrayList<>();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_notification, container, false);
 
+        getNotify(Config.URL_HOST + Config.URL_GET_ALL_EVENTS, view);
+
         return view;
+    }
+
+    private void getNotify(String url, View view) {
+
+        final RecyclerView recyclerView;
+        recyclerView = view.findViewById(R.id.RecyclerView_EventList);
+        recyclerView.setHasFixedSize(true); //Tối ưu hóa dữ liệu, k bị ảnh hưởng bởi nội dung trong adapter
+
+        LinearLayoutManager linearLayoutManager =
+                new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        ArrayList<Event> events = new ModelEvent().getEventList(getContext(), url);
+
+        final EventAdapter eventAdapter = new EventAdapter(recyclerView, events, getContext());
+        recyclerView.setAdapter(eventAdapter);
+        eventAdapter.notifyDataSetChanged();
+
+        //set load more listener for the RecyclerView adapter
+        final ArrayList<Event> finalListService = events;
+        try {
+            finalArr = JsonHelper.parseJsonNoId(new JSONObject
+                    (new HttpRequestAdapter.httpGet().execute(url).get()), Config.GET_KEY_JSON_LOAD);
+        } catch (JSONException | InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        eventAdapter.setOnLoadMoreListener(new OnLoadMoreListener() {
+
+            @Override
+            public void onLoadMore() {
+                if (finalListService.size() < Integer.parseInt(finalArr.get(2))) {
+                    finalListService.add(null);
+                    recyclerView.post(new Runnable() {
+                        public void run() {
+                            eventAdapter.notifyItemInserted(finalListService.size() - 1);
+                        }
+                    });
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            finalListService.remove(finalListService.size() - 1);
+                            eventAdapter.notifyItemRemoved(finalListService.size());
+
+                            ArrayList<Event> serviceArrayList = new ModelEvent().
+                                    getEventList(getApplicationContext(), finalArr.get(1));
+                            finalListService.addAll(serviceArrayList);
+                            try {
+                                finalArr = JsonHelper.parseJsonNoId(new JSONObject
+                                        (new HttpRequestAdapter.httpGet().execute(finalArr.get(1)).get()),
+                                        Config.GET_KEY_JSON_LOAD);
+                            } catch (JSONException | InterruptedException | ExecutionException e) {
+                                e.printStackTrace();
+                            }
+
+                            eventAdapter.notifyDataSetChanged();
+                            eventAdapter.setLoaded();
+                        }
+                    }, 1000);
+                }
+            }
+        });
     }
 }
